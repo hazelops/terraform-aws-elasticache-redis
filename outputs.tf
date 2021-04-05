c20893ebc85a5c16a143d148e77d3b540fa1ad24
igorkotof <igor.kotov@hazelops.com>
2021-04-05 21:29:29 +0300

    fixed outputs

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "elasticache_replication_group_id" {
-  value       = join("", aws_elasticache_replication_group.default.id)
+  value       = aws_elasticache_replication_group.default.id
   description = "The ID of the ElastiCache Replication Group."
 }
 
 output "elasticache_replication_group_primary_endpoint_address" {
-  value       = join("", aws_elasticache_replication_group.default.primary_endpoint_address)
+  value       = aws_elasticache_replication_group.default.primary_endpoint_address
   description = "The address of the endpoint for the primary node in the replication group."
 }
 
@@ -14,37 +14,37 @@ output "elasticache_replication_group_member_clusters" {
 }
 
 output "elasticache_parameter_group_id" {
-  value       = join("", aws_elasticache_parameter_group.default.id)
+  value       = aws_elasticache_parameter_group.default.id
   description = "The ElastiCache parameter group name."
 }
 
 output "security_group_id" {
-  value       = join("", aws_security_group.default.id)
+  value       = aws_security_group.default.id
   description = "The ID of the Redis ElastiCache security group."
 }
 
 output "security_group_arn" {
-  value       = join("", aws_security_group.default.arn)
+  value       = aws_security_group.default.arn
   description = "The ARN of the Redis ElastiCache security group."
 }
 
 output "security_group_vpc_id" {
-  value       = join("", aws_security_group.default.vpc_id)
+  value       = aws_security_group.default.vpc_id
   description = "The VPC ID of the Redis ElastiCache security group."
 }
 
 output "security_group_owner_id" {
-  value       = join("", aws_security_group.default.owner_id)
+  value       = aws_security_group.default.owner_id
   description = "The owner ID of the Redis ElastiCache security group."
 }
 
 output "security_group_name" {
-  value       = join("", aws_security_group.default.name)
+  value       = aws_security_group.default.name
   description = "The name of the Redis ElastiCache security group."
 }
 
 output "security_group_description" {
-  value       = join("", aws_security_group.default.description)
+  value       = aws_security_group.default.description
   description = "The description of the Redis ElastiCache security group."
 }
 
